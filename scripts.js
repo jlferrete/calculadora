e2b74a7bbec7f0527c30549d42adee1d1f0db124
JlFerrete <joseluisferrete@gmail.com>
2019-08-17 22:04:13 +0200

    JS problems

--- scripts.js
+++ scripts.js
@@ -4,7 +4,13 @@ function getContentClick(event) {
 }
 
 const filterAction = value => {
-  console.log("Estamos en filterAction");
+  value === "0" ? addNumberInput(0) : null;
+}
+
+function addNumberInput(value) {
 
   console.log(value);
-};
+  const inputScreen = document.getElementsByClassName('calculator__screen')[0];
+
+  inputScreen.value = value;
+}
